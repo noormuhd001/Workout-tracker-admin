63e79e628f4121dbed611cff8eaa6bdee0abd0f8
Noor muhammed <noormuhammed@bytekat.com>
2026-06-04 11:52:53 +0530

    added part filtering in workout listing page

--- src/Pages/workout.jsx
+++ src/Pages/workout.jsx
@@ -4,6 +4,17 @@ import "../styles/workout.css";
 
 export default function Workouts() {
   const [workouts, setWorkouts] = useState([]);
+  const [selectedPart, setSelectedPart] = useState("All");
+
+  const bodyParts = [
+    "All",
+    ...new Set(workouts.map((workout) => workout.body_part)),
+  ];
+
+  const filteredWorkouts =
+    selectedPart === "All"
+      ? workouts
+      : workouts.filter((workout) => workout.body_part === selectedPart);
 
   useEffect(() => {
     const fetchWorkouts = async () => {
@@ -19,17 +30,28 @@ export default function Workouts() {
   return (
     <div>
       <Navbar></Navbar>
-            <div className="container py-4">
-      <div className ="row">
-        {workouts.map((workout, index) => (
-          <div key={index} className="col-md-4 workout-card">
-            <img src={workout.image_url} alt={workout.workout_name} />
-            <h3>{workout.workout_name}</h3>
-          </div>
-        ))}
-      </div>
+      <div className="container py-4">
+        <div className="filter-container mb-4">
+          {bodyParts.map((part) => (
+            <button
+              key={part}
+              className={`filter-btn ${selectedPart === part ? "active" : ""}`}
+              onClick={() => setSelectedPart(part)}
+            >
+              {part}
+            </button>
+          ))}
+        </div>
+        <div className="row">
+          {filteredWorkouts.map((workout) => (
+            <div key={workout.id} className="col-md-4 workout-card">
+              <img src={workout.image_url} alt={workout.workout_name} />
+              <h3>{workout.workout_name}</h3>
+              <p>{workout.body_part}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
 }
- 
